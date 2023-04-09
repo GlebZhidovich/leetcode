26d9a961d51e2ec56694abd219c64c1083235616
GlebZhidovich <gzhidovich@gmail.com>
2023-04-09 10:59:28 +0300

    feat: add better solution for numIslands

--- 200. Number of Islands/index.js
+++ 200. Number of Islands/index.js
@@ -3,15 +3,6 @@
  * @return {number}
  */
 var numIslands = function (grid) {
-  const q = [];
-  grid.forEach((row, i) => {
-    row.forEach((n, j) => {
-      if (n === "1") {
-        q.push([i, j]);
-      }
-    });
-  });
-
   const options = [
     [0, 1],
     [1, 0],
@@ -23,50 +14,52 @@ var numIslands = function (grid) {
 
   let count = 0;
 
-  for (let i = 0; i < q.length; i++) {
-    const cors = q[i];
-    const t = `${cors[0]} ${cors[1]}`;
+  grid.forEach((row, i) => {
+    row.forEach((n, j) => {
+      const t = `${i} ${j}`;
 
-    if (!visited.has(t)) {
-      count++;
-      visited.add(t);
-      let queue = [cors];
-      let nextQueue = [];
+      if (n === "1" && !visited.has(t)) {
+        const cors = [i, j];
+        count++;
+        visited.add(t);
+        let queue = [cors];
+        let nextQueue = [];
 
-      for (let index = 0; index < queue.length; ) {
-        const [i, j] = queue[index];
+        for (let index = 0; index < queue.length; ) {
+          const [i, j] = queue[index];
 
-        options.forEach(([a, b]) => {
-          const y = i + a;
-          const x = j + b;
-          if (grid[y] && grid[y][x] === "1") {
-            const key = `${y} ${x}`;
-            if (!visited.has(key)) {
-              visited.add(key);
-              nextQueue.push([y, x]);
+          options.forEach(([a, b]) => {
+            const y = i + a;
+            const x = j + b;
+            if (grid[y] && grid[y][x] === "1") {
+              const key = `${y} ${x}`;
+              if (!visited.has(key)) {
+                visited.add(key);
+                nextQueue.push([y, x]);
+              }
             }
-          }
-        });
+          });
 
-        if (index == queue.length - 1 && nextQueue.length) {
-          index = 0;
-          queue = nextQueue;
-          nextQueue = [];
-        } else {
-          index++;
+          if (index == queue.length - 1 && nextQueue.length) {
+            index = 0;
+            queue = nextQueue;
+            nextQueue = [];
+          } else {
+            index++;
+          }
         }
       }
-    }
-  }
+    });
+  });
 
   return count;
 };
 
 const grid = [
-  ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
+  ["1", "1", "0", "0", "0"],
+  ["1", "1", "0", "0", "0"],
+  ["0", "0", "1", "0", "0"],
+  ["0", "0", "0", "1", "1"],
 ];
 const res = numIslands(grid);
 console.log("🚀 ~ file: index.js:17 ~ res:", res);
